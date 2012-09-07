2d2098b57c5d035de78473f0ffe8a3a1ef1bae46
ikeikeikeike <jp.ne.co.jp@gmail.com>
2012-09-08 03:56:52 +0900

    Adding celeryconfig.

---
+++ tracker/configs/celeryconfig.py
@@ -0,0 +1,52 @@
+"""
+Global Settings
+
+"""
+settings = {}
+# try:
+    # conf = __import__("django.conf")
+    # settings = conf.conf.settings
+# except ImportError:
+    # settings = {}
+
+
+# Broker - amqp,
+BROKER_URL = getattr(settings, "BROKER_URL", "amqp://guest@127.0.0.1//")
+
+
+# file storage path (default: memory),  e.g. /tmp/tracker.db
+CELERY_TRACKER_STORAGE = getattr(settings, "CELERY_TRACKER_STORAGE", "")
+
+
+# Log level
+CELERY_TRACKER_LOG_LEVEL = getattr(settings, "CELERY_TRACKER_LOG_LEVEL", "INFO")
+
+
+# plugins
+CELERY_TRACKER_PLUGINS = getattr(settings, "CELERY_TRACKER_PLUGINS", {
+    "fluent": {
+        "class": "tracker.plugins.fluent.FluentPlugin",
+        "verbose": 0,
+        "interval": 20,
+        "tag": "celery.tracker",
+        "host": "127.0.0.1",
+        "port": 24224
+    },
+    "zabbix": {
+        "class": "tracker.plugins.zabbix.ZabbixPlugin",
+        "verbose": 0,
+        "interval": 20,
+        "tag": "celery.tracker",
+        "host": "127.0.0.1",
+        "port": 10051,
+        "metrics": [
+            {"host": "celery-agent"},
+        ]
+    },
+    #"logging": {
+    #    "class": "tracker.plugins.logging.LoggingPlugin",
+    #    "tag": "celery.tracker",
+    #    "interval": 10,
+    #    "verbose": True
+    #},
+})
