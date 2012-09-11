4e95308da860b968163f92b4e5365407b1b34c19
ikeikeikeike <jp.ne.co.jp@gmail.com>
2012-09-12 02:27:26 +0900

    Bump version.

--- tracker/__init__.py
+++ tracker/__init__.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 
-VERSION = (0, 2, 3, "beta", 8)
+VERSION = (0, 2, 4, "beta", 4)
 __version__ = '.'.join(map(str, VERSION[0:3]))
 
 
